b0a8486233a0f1debfd62ec21d45a719450fd5c6
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-09-17 14:00:52 +0900

    終了

---
+++ techpit-match/db/migrate/20210915032006_add_gender_and_name_to_user.rb
@@ -0,0 +1,6 @@
+class AddGenderAndNameToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :gender, :integer, null:false, default:0
+    add_column :users, :name, :string, null:false
+  end
+end
